8a99d5a2b80d0cc8b11a73e81ac2d82586b75805
molBr <pedromollica@gmail.com>
2021-07-24 11:00:44 -0300

    fix de info

--- src/ShowQuestion/showQuestion.js
+++ src/ShowQuestion/showQuestion.js
@@ -4,8 +4,10 @@ const ShowQuestion = () => {
         <div>
             <h3>Github: </h3>
             <a href="https://github.com/molBR">https://github.com/molBR</a>
-            <h3>Linkedin: https://www.linkedin.com/in/pedro-vallese/</h3>
+            <h3>Linkedin:</h3>
             <a href="https://www.linkedin.com/in/pedro-vallese/">https://www.linkedin.com/in/pedro-vallese/</a>
+            <h3>Discord:</h3>
+            <a href="https://discord.gg/SNAwqs2v">https://discord.gg/SNAwqs2v</a>
             
         </div>
 
